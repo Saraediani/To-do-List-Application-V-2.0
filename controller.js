0c7f1d7392bcdb4d38f45725fe3669bec34e7fd4
saraediani <sara.pur187@gmail.com>
2021-12-20 12:17:28 +0100

    method get

---
+++ controller.js
@@ -0,0 +1,11 @@
+// Logic behind the functionalities
+const data = require("./data");
+
+class Controller {
+    // getting all todos
+    async getTodos() {
+        // return all todos
+        return new Promise((resolve, _) => resolve(data));
+    }
+}
+module.exports = Controller;
--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const http = require('http');
+// const Todos = require("./controller");
+const todo = require('./data');
 const port = 3000;
 const server = http.createServer((req, res) => {
-    console.log(req.headers);
-    res.statusCode = 200;
-    res.end('<html><body><h1>Hello, World!</h1></body></html>');
+    if (req.url === "/api" && req.method === "GET") {
+        res.writeHead(200, { "Content-Type": "application/json" });
+        res.end(JSON.stringify(todo));
+    } else {
+        res.writeHead(404, { "Content-Type": "application/json" });
+        res.end(JSON.stringify({ message: "Route not found" }));
+    }
 })
 server.listen(port, () => {
     console.log(`app is live ${port}`);
